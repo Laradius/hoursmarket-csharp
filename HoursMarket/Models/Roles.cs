a9cf0b7b136f27f4c31cb0058d03ebd9a1b9fb4c
Laradius <loganl33t@gmail.com>
2020-07-26 00:26:23 +0200

    Account model and roles

---
+++ HoursMarket/Models/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoursMarket.Models
+{
+    public enum Roles
+    {
+        User = 0,
+        Manager = 1,
+        Administrator = 2
+
+    }
+}
