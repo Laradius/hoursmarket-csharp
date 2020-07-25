594c76a1745f587af7611aa7e656d0a22fb6e1bc
Laradius <loganl33t@gmail.com>
2020-07-26 00:16:26 +0200

    Init commit

---
+++ HoursMarket/Controllers/HoursController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HoursMarket.Controllers
+{
+    [ApiController]
+    public class HoursController : ControllerBase
+    {
+
+
+
+
+    }
+}
+
---
+++ HoursMarket/Models/HourOffer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoursMarket.Models
+{
+    public class HourOffer
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BeginDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+
+    }
+}
